2a155bd0f8ad2c0fb4abd0dea82339c3feb709d7
iammeghana <mkoramut@gmu.edu>
2024-05-13 20:17:18 -0400

    cypress/e2e/spec.cy.js

--- cypress/cypress/e2e/spec.cy.js
+++ cypress/cypress/e2e/spec.cy.js
@@ -35,19 +35,23 @@ describe('Write Password into .secret', () => {
 
 describe('Create Python process and run it', () => {
   it('creates python process and runs', () => {
+    cy.task('log', 'In creating python process and running it' );
     cy.visit('http://localhost:8070/Geoweaver');
     cy.get('.introjs-skipbutton').click();
     cy.get('#newprocess').click();
 
     cy.get('form select.form-control.form-control-sm').select('Python');
     cy.get('form > :nth-child(1) > :nth-child(4)').type('hello_world.py');
-
+    cy.task('log', 'just created and named new process as hello_world.py' );
 
     cy.get('.CodeMirror-lines').type("\nprint('hello world!')");
+    cy.task('log', 'Code is written to the monitor' );
     cy.get('.modal-footer').contains('Add').click();
 
+    cy.task('log', 'Added the process' );
     cy.get('ul#process_folder_python_target').contains('hello_world.py').click();
 
+    cy.task('log', 'hello_world.py process is created completely' );
     cy.get('#processid').then(($input) => {
       const processId = $input.val(); // Get the value of the input field
       console.log('process id ',processId)
@@ -56,11 +60,14 @@ describe('Create Python process and run it', () => {
     });
    
     cy.get('#host-execute-btn').click();
-
+    cy.task('log', 'Clicked on execute button' );
     cy.get('#process-confirm-btn').click();
+    cy.task('log', 'Password is confirmed' );
     cy.get('#inputpswd').clear('1');
+    cy.task('log', 'Inputting the password' );
     cy.get('#inputpswd').type('1234');
     cy.get('#pswd-confirm-btn').click();
+    cy.task('log', 'Passowrd confirm is done' );
     cy.intercept('POST','/Geoweaver/web/executeProcess').as('executeProcess'); 
     cy.wait('@executeProcess').its('response.statusCode').should('eq', 200);
     // cy.get('#single-console-content').should('contain', 'hello world!');
@@ -73,71 +80,71 @@ describe('Create Python process and run it', () => {
 
 
 
-describe('Hosts Testing', () => {
-  it('Create New Host', () => {
-    cy.visit('http://localhost:8070/Geoweaver/web/geoweaver');
-    cy.get('.introjs-skipbutton').click();
-    cy.get('#newhost').click();
-    cy.get('#hostip').clear('1');
-    cy.get('#hostip').type('1.1.1.1');
-    cy.get('#hostport').clear('2');
-    cy.get('#hostport').type('22');
-    cy.get('#username').clear('n');
-    cy.get('#username').type('newuser');
-    cy.get('#host-add-btn').click();
-    cy.get('ul#host_folder_ssh_target').should('contain', 'New Host');
-  }) 
+// describe('Hosts Testing', () => {
+//   it('Create New Host', () => {
+//     cy.visit('http://localhost:8070/Geoweaver/web/geoweaver');
+//     cy.get('.introjs-skipbutton').click();
+//     cy.get('#newhost').click();
+//     cy.get('#hostip').clear('1');
+//     cy.get('#hostip').type('1.1.1.1');
+//     cy.get('#hostport').clear('2');
+//     cy.get('#hostport').type('22');
+//     cy.get('#username').clear('n');
+//     cy.get('#username').type('newuser');
+//     cy.get('#host-add-btn').click();
+//     cy.get('ul#host_folder_ssh_target').should('contain', 'New Host');
+//   }) 
 
-  it('Delete New Host', () => {
-    cy.visit('http://localhost:8070/Geoweaver/web/geoweaver');
-    cy.get('.introjs-skipbutton').click();
-    cy.get('#host_folder_ssh > a').click();
-    cy.get('ul#host_folder_ssh_target').contains('New Host').click();
-    cy.get('.fa-minus').click();
-    cy.get('#del-confirm-btn').click();
-  })
+//   it('Delete New Host', () => {
+//     cy.visit('http://localhost:8070/Geoweaver/web/geoweaver');
+//     cy.get('.introjs-skipbutton').click();
+//     cy.get('#host_folder_ssh > a').click();
+//     cy.get('ul#host_folder_ssh_target').contains('New Host').click();
+//     cy.get('.fa-minus').click();
+//     cy.get('#del-confirm-btn').click();
+//   })
 
-  it('LocalHost testing', () => {
-    cy.visit('http://localhost:8070/Geoweaver/web/geoweaver');
-    cy.get('.introjs-skipbutton').click();
-    cy.get('#host_folder_ssh > a').click();
-    cy.get('#host-100001').click();
-    cy.get('#_host_name').should('have.value', 'Localhost');
-    cy.get('#_host_ip').should('have.value', '127.0.0.1');
-    cy.get('#_host_port').should('have.value', '22');
-    cy.get('#_host_username').should('have.value', 'publicuser');
-    cy.get('#_host_url').should('have.value', 'http://localhost/');
-    cy.get('#_host_type').click();
-    cy.get('#_host_type').should('have.text', 'ssh');
-  })
+//   it('LocalHost testing', () => {
+//     cy.visit('http://localhost:8070/Geoweaver/web/geoweaver');
+//     cy.get('.introjs-skipbutton').click();
+//     cy.get('#host_folder_ssh > a').click();
+//     cy.get('#host-100001').click();
+//     cy.get('#_host_name').should('have.value', 'Localhost');
+//     cy.get('#_host_ip').should('have.value', '127.0.0.1');
+//     cy.get('#_host_port').should('have.value', '22');
+//     cy.get('#_host_username').should('have.value', 'publicuser');
+//     cy.get('#_host_url').should('have.value', 'http://localhost/');
+//     cy.get('#_host_type').click();
+//     cy.get('#_host_type').should('have.text', 'ssh');
+//   })
 
-  it('LocalHost Read Python Env', () => {
-    cy.visit('http://localhost:8070/Geoweaver/web/geoweaver');
-    cy.get('.introjs-skipbutton').click();
-    cy.get('#host_folder_ssh > a').click();
-    cy.get('#host-100001').click();
-    cy.get('.fab').click();
-    cy.get('#inputpswd').clear('1');
-    cy.get('#inputpswd').type('1234');
-    cy.get('#pswd-confirm-btn').click();
-    cy.intercept('POST', '/Geoweaver/web/readEnvironment').as('readEnvironment');
-    cy.wait('@readEnvironment').then((interception) => {
-      expect(interception.response.statusCode).to.equal(200);
-    });
-  })
+//   it('LocalHost Read Python Env', () => {
+//     cy.visit('http://localhost:8070/Geoweaver/web/geoweaver');
+//     cy.get('.introjs-skipbutton').click();
+//     cy.get('#host_folder_ssh > a').click();
+//     cy.get('#host-100001').click();
+//     cy.get('.fab').click();
+//     cy.get('#inputpswd').clear('1');
+//     cy.get('#inputpswd').type('1234');
+//     cy.get('#pswd-confirm-btn').click();
+//     cy.intercept('POST', '/Geoweaver/web/readEnvironment').as('readEnvironment');
+//     cy.wait('@readEnvironment').then((interception) => {
+//       expect(interception.response.statusCode).to.equal(200);
+//     });
+//   })
 
-  it('LocalHost File Upload', () => {
-    cy.visit('http://localhost:8070/Geoweaver/web/geoweaver');
-    cy.get('.introjs-skipbutton').click();
-    cy.get('#host_folder_ssh > a').click();
-    cy.get('#host-100001').click();
-    cy.get('p > .fa-upload').click();
-    cy.get('#inputpswd').clear('1');
-    cy.get('#inputpswd').type('1234');
-    cy.get('#pswd-confirm-btn').click();
-    cy.get('#host-file-uploader').click();
-    cy.intercept('POST', 'http://localhost:8070/Geoweaver/web/authenticateUser').as('authenticateUser');
-    cy.wait('@authenticateUser').its('response.statusCode').should('eq', 200);
-  })
+//   it('LocalHost File Upload', () => {
+//     cy.visit('http://localhost:8070/Geoweaver/web/geoweaver');
+//     cy.get('.introjs-skipbutton').click();
+//     cy.get('#host_folder_ssh > a').click();
+//     cy.get('#host-100001').click();
+//     cy.get('p > .fa-upload').click();
+//     cy.get('#inputpswd').clear('1');
+//     cy.get('#inputpswd').type('1234');
+//     cy.get('#pswd-confirm-btn').click();
+//     cy.get('#host-file-uploader').click();
+//     cy.intercept('POST', 'http://localhost:8070/Geoweaver/web/authenticateUser').as('authenticateUser');
+//     cy.wait('@authenticateUser').its('response.statusCode').should('eq', 200);
+//   })
 
-});
+// });
