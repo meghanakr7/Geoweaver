f3e68616e1f5274c55f610bdab76c281d12c68c0
iammeghana <mkoramut@gmu.edu>
2024-05-21 17:51:53 -0400

    check_tests

--- cypress/cypress/e2e/spec.cy.js
+++ cypress/cypress/e2e/spec.cy.js
@@ -322,40 +322,6 @@ describe('Edit Process Name', () => {
 // });
 
 describe('Create Python process and run it', () => {
-  it('creates python process and runs', () => {
-    cy.visit('http://localhost:8070/Geoweaver');
-    cy.get('.introjs-skipbutton').click();
-    cy.get('#newprocess').click();
-
-    cy.get('form select.form-control.form-control-sm').select('Python');
-    cy.get('form > :nth-child(1) > :nth-child(4)').type('h_test.py');
-
-    cy.get('.view-lines').invoke('text', "\nprint('hello world!')");
- 
-    cy.get('.modal-footer').contains('Add').click();
-
-    cy.get('ul#process_folder_python_target').contains('h_test.py').click();
-
-    cy.get('#processid').then(($input) => {
-      const processId = $input.val(); 
-      console.log('process id ',processId)
-      const selector = `[onclick="GW.process.runProcess('${processId}', 'h_test.py', 'python')"]`;
-      cy.get(selector).click(); 
-    });
-    cy.intercept('POST', '/Geoweaver/web/executeProcess').as('executeProcess');
-    cy.get('#host-execute-btn').click();
-
-    cy.get('#process-confirm-btn').click();
-    cy.get('#inputpswd').clear('1');
-    cy.get('#inputpswd').type('1234');
-    cy.get('#pswd-confirm-btn').click();
-
-    cy.get('#single-console-content').should('contain', 'hello world!');
-    cy.get('#single-console-content').should('contain', 'Exit Code: 0');
-    cy.get('#process-log-window').click();
-    cy.get('#process-log-window').should('be.visible');
-    cy.wait('@executeProcess').its('response.statusCode').should('eq', 200);
-  });
 
   it('creates python process and runs test', function() {
     /* ==== Generated with Cypress Studio ==== */
@@ -377,20 +343,6 @@ describe('Create Python process and run it', () => {
     cy.get('#process-log-window > :nth-child(3) > span').should('be.visible');
     /* ==== End Cypress Studio ==== */
   });
-  // it('test', () => {
-  //   /* ==== Generated with Cypress Studio ==== */
-  //   cy.visit('http://localhost:8070/Geoweaver/web/geoweaver');
-  //   cy.get('.introjs-skipbutton').click();
-  //   cy.get('#newprocess').click();
-
-  //   cy.get('form select.form-control.form-control-sm').select('Python');
-  //   cy.get('form > :nth-child(1) > :nth-child(4)').type('hello_world.py');
-  //   cy.get('.view-lines').click();
-  //   cy.get('.modal-footer').click();
-  //   cy.get('#add-process-206').click();
-  //   cy.get('#process-xd04rh > .row > .col-md-8').should('be.visible');
-  //   /* ==== End Cypress Studio ==== */
-  // });
 });
 
 
